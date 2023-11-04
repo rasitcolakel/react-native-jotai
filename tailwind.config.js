47383d74d2297a107f83d9069cb38e31d610f4b6
Raşit Çolakel <rasitcolakel@hotmail.com>
2023-11-04 11:41:01 +0300

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.{js,ts,tsx}",
+    "./app/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
